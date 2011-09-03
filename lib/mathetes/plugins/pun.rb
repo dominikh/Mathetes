b29e3e4cc76b1ea18b35ca001538ee9c178032e9
Dominik Honnef <dominikh@fork-bomb.org>
2011-09-03 18:27:40 +0200

    port Pun plugin to cinch

--- lib/mathetes/plugins/pun.rb
+++ lib/mathetes/plugins/pun.rb
@@ -1,17 +1,16 @@
 require 'open-uri'
 require 'nokogiri'
 
-module Mathetes; module Plugins
+module Cinch
+  module Plugins
+    class Pun
+      include Cinch::Plugin
 
-  class Pun
-
-    def initialize( mathetes )
-      mathetes.hook_privmsg( :regexp => /^!pun\b/ ) do |message|
+      match("pun")
+      def execute(m)
         doc = Nokogiri::HTML( open( "http://www.punoftheday.com/cgi-bin/randompun.pl" ) )
-        message.answer doc.search( '#main-content p' )[ 0 ].inner_text
+        m.reply doc.search( '#main-content p' )[ 0 ].inner_text
       end
     end
-
   end
-
-end; end
+end
