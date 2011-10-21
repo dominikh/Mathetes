c7a25830b8ce3464666189dc6f4bbe1a3a6978f8
Dominik Honnef <dominikh@fork-bomb.org>
2011-10-22 01:03:43 +0200

    port spell plugin to cinch

--- lib/cinch/plugins/spell.rb
+++ lib/cinch/plugins/spell.rb
@@ -1,63 +1,38 @@
-module Mathetes; module Plugins
+module Cinch
+  module Plugins
+    class Spell
+      ValidLanguages = %w[en de fr pt es it]
+      include Cinch::Plugin
 
-  class Spell
-
-    MAX_WORD_LENGTH = 50
-    NUM_SUGGESTIONS = 15
-
-    def initialize( mathetes )
-      mathetes.hook_privmsg( :regexp => /^!spell\b/ ) do |message|
-        catch :done do
-          rest = message.text[ /^\S+\s+(.*)/, 1 ]
-          if rest.nil?
-            message.answer "!spell [language code] word"
-            throw :done
-          end
-
-          args = rest.split( /\s+/ )
-          language = "en"
-          word = nil
-
-          case args.length
-          when 1
-            word = args[ 0 ]
-          else
-            lang = args[ 0 ].downcase
-            word = args[ 1 ]
-            case lang
-            when "en", "de", "fr", "pt", "es", "it"
-              language = lang
-            end
-          end
-
-          throw :done  if word.nil?
-
-          if word.length > MAX_WORD_LENGTH
-            retval = "That's not a real word!  :P"
-          else
-            word.gsub!( /[^a-zA-Z'-]/, '' )
-            aspell = `echo '#{word.gsub("'", "\\'")}' | aspell -d '#{language}' -a --sug-mode=bad-spellers`
-
-            list = aspell.split( ':' )
-            result = list[ 0 ]
-
-            if result =~ /\*$/
-              retval = "#{word} is spelled correctly."
-            else
-              if list[ 1 ]
-                words = list[ 1 ].strip.split( "," )
-                retval = "'#{word}' is probably one of: #{words[ 0, NUM_SUGGESTIONS ].join( ',' )}"
-              else
-                retval = "No suggestions for unknown word '#{word}'."
-              end
-            end
-          end
-
-          message.answer retval
+      match(/spell (?:([\S]+) )?(.+)/)
+      def execute(m, lang, word)
+        unless ValidLanguages.include?(lang)
+          lang = "en"
         end
+
+        if word.length > config[:max_word_length]
+          retval = "That's not a real word!  :P"
+        else
+          word.gsub!( /[^a-zA-Z'-]/, '' )
+          aspell = `echo '#{word.gsub("'", "'\\\\''")}' | aspell -d '#{language}' -a --sug-mode=bad-spellers`
+
+          list = aspell.split( ':' )
+          result = list[ 0 ]
+
+          if result =~ /\*$/
+            retval = "#{word} is spelled correctly."
+          else
+            if list[ 1 ]
+              words = list[ 1 ].strip.split( "," )
+              retval = "'#{word}' is probably one of: #{words[ 0, config[:num_suggestions] ].join( ',' )}"
+            else
+              retval = "No suggestions for unknown word '#{word}'."
+            end
+          end
+        end
+
+        m.reply retval
       end
     end
-
   end
-
-end; end
+end
