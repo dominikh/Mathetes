79c1c5a1efb5e39d6e35013cc545f8945ca949bd
Dominik Honnef <dominikh@fork-bomb.org>
2011-10-21 23:01:06 +0200

    port ideone plugin to cinch

--- lib/mathetes/plugins/ideone.rb
+++ lib/mathetes/plugins/ideone.rb
@@ -1,16 +1,14 @@
 require 'ideone'
 
-module Mathetes; module Plugins
+module Cinch
+  module Plugins
+    class Ideone
+      include Cinch::Plugin
 
-  class Ideone
-
-    MAX_RESULT_LENGTH = 256   # characters
-
-    def initialize( mathetes )
-      mathetes.hook_privmsg( :regexp => /^!(?:ruby|rb|rbp|python|py|perl|pl|php)\b/ ) do |message|
-        message.text =~ /^!(\S+)\s+(.*)/
-        lang_, code = $1, $2
-        case lang_
+      match(/(ruby|rb|rbp|python|py|perl|pl|php) (.+)/)
+      def execute(m, lang, code)
+        mathetes.hook_privmsg( :regexp => /^!(?:ruby|rb|rbp|python|py|perl|pl|php)\b/ ) do |message|
+          case lang
           when 'ruby', 'rb'
             lang = :ruby
           when 'rbp'
@@ -27,21 +25,21 @@ module Mathetes; module Plugins
             lang = :perl
           when 'php'
             lang = :php
-        end
-
-        paste_id = ::Ideone.submit( lang, code )
-        begin
-          stdout = ::Ideone.run( paste_id, nil ).inspect
-          if stdout.length > MAX_RESULT_LENGTH
-            stdout = stdout[ 0...MAX_RESULT_LENGTH ] + "..."
           end
-          message.answer "[code] #{stdout}"
-        rescue ::Ideone::IdeoneError => e
-          message.answer "[code] #{e.message}"
+
+          paste_id = ::Ideone.submit( lang, code )
+          begin
+            stdout = ::Ideone.run( paste_id, nil ).inspect
+            if stdout.length > config[:max_result_length]
+              stdout = stdout[ 0...config[:max_result_length] ] + "..."
+            end
+            m.reply "[code] #{stdout}"
+          rescue ::Ideone::IdeoneError => e
+            m.reply "[code] #{e.message}"
+          end
         end
       end
     end
-
   end
+end
 
-end; end
