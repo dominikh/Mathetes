286ce50b74d8771dfee595f5865a7ed39fa9b506
Dominik Honnef <dominikh@fork-bomb.org>
2011-10-21 23:24:07 +0200

    port key value store pluginto cinch

--- lib/cinch/plugins/key-value-store.rb
+++ lib/cinch/plugins/key-value-store.rb
@@ -1,35 +1,39 @@
 require 'mutex-pstore'
 
-module Mathetes; module Plugins
+module Cinch
+  module Plugins
+    class KeyValueStore
+      include Cinch::Plugin
 
-  class KeyValueStore
+      def initialize(*args)
+        super
+        @h = MuPStore.new( "key-value.pstore" )
+      end
 
-    def initialize( mathetes )
-      @h = MuPStore.new( "key-value.pstore" )
-      mathetes.hook_privmsg( :regexp => /^!i(nfo)?\b/ ) do |message|
-        if message.text =~ /^\S+\s+(.+?)=(.+)/
+      help "Usage: !i key = value     !i key"
+      match(/i(?:nfo)? (.+)/)
+      def execute(m, rest)
+        if rest =~ /^\S+ (.+?)=(.+)/
           key, value = $1.strip, $2.strip
           @h.transaction {
-            @h[ { :channel => message.channel.to_s, :key => key }.inspect ] = value
+            @h[ { :channel => m.channel.name, :key => key }.inspect ] = value
           }
-          message.answer "Set '#{key}'."
-        elsif message.text =~ /^\S+\s+(.+)/
+          m.reply "Set '#{key}'."
+        elsif rest =~ /^\S+\s+(.+)/
           key = $1.strip
           value = nil
           @h.transaction {
-            value = @h[ { :channel => message.channel.to_s, :key => key }.inspect ]
+            value = @h[ { :channel => m.channel.name, :key => key }.inspect ]
           }
           if value
-            message.answer value
+            m.reply value
           else
-            message.answer "No value for key '#{key}'."
+            m.reply "No value for key '#{key}'."
           end
         else
-          message.answer "Usage: !i key = value    !i key"
+          m.reply "Usage: !i key = value    !i key"
         end
       end
     end
-
   end
-
-end; end
+end
