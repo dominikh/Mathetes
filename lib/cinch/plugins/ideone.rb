259778f865fa1d8880ee4f084d902d17f046c63d
Dominik Honnef <dominikh@fork-bomb.org>
2011-10-22 02:39:26 +0200

    fix ideone plugin

--- lib/cinch/plugins/ideone.rb
+++ lib/cinch/plugins/ideone.rb
@@ -7,36 +7,34 @@ module Cinch
 
       match(/(ruby|rb|rbp|python|py|perl|pl|php) (.+)/)
       def execute(m, lang, code)
-        mathetes.hook_privmsg( :regexp => /^!(?:ruby|rb|rbp|python|py|perl|pl|php)\b/ ) do |message|
-          case lang
-          when 'ruby', 'rb'
-            lang = :ruby
-          when 'rbp'
-            lang = :ruby
-            code = %{
+        case lang
+        when 'ruby', 'rb'
+          lang = :ruby
+        when 'rbp'
+          lang = :ruby
+          code = %{
               def print_wrapper__
                 #{code}
               end
               print print_wrapper__.inspect
             }
-          when 'python', 'py'
-            lang = :python
-          when 'perl', 'pl'
-            lang = :perl
-          when 'php'
-            lang = :php
-          end
+        when 'python', 'py'
+          lang = :python
+        when 'perl', 'pl'
+          lang = :perl
+        when 'php'
+          lang = :php
+        end
 
-          paste_id = ::Ideone.submit( lang, code )
-          begin
-            stdout = ::Ideone.run( paste_id, nil ).inspect
-            if stdout.length > config[:max_result_length]
-              stdout = stdout[ 0...config[:max_result_length] ] + "..."
-            end
-            m.reply "[code] #{stdout}"
-          rescue ::Ideone::IdeoneError => e
-            m.reply "[code] #{e.message}"
+        paste_id = ::Ideone.submit( lang, code )
+        begin
+          stdout = ::Ideone.run( paste_id, nil ).inspect
+          if stdout.length > config[:max_result_length]
+            stdout = stdout[ 0...config[:max_result_length] ] + "..."
           end
+          m.reply "[code] #{stdout}"
+        rescue ::Ideone::IdeoneError => e
+          m.reply "[code] #{e.message}"
         end
       end
     end
