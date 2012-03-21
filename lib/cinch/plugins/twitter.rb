5a86d31897662432777f68e6515711ccf176e0b3
Dominik Honnef <dominikh@fork-bomb.org>
2012-03-21 11:48:44 +0100

    fix twitter plugin

--- lib/cinch/plugins/twitter.rb
+++ lib/cinch/plugins/twitter.rb
@@ -16,23 +16,17 @@ module Cinch
       def initialize(*args)
         super
 
-        @twitter = ::Twitter::Base.new(::Twitter::HTTPAuth.new( config[ 'username' ], config[ 'password' ] ))
-        @last_search_id = Hash.new
-        @seen = Hash.new { |hash,key| hash[ key ] = Array.new }
-
-        config[:searches].each do |search_term,channels|
-          search = ::Twitter::Search.new( search_term )
-          rescue_exception do
-            fetched = search.fetch
-            max_id = fetched[ 'max_id' ].to_i
-            @last_search_id[ search_term ] = max_id
-            channels.each do |channel|
-              @seen[ channel ] << max_id
-            end
-          end
+        ::Twitter.configure do |c|
+          c.consumer_key       = config[:consumer_key]
+          c.consumer_secret    = config[:consumer_secret]
+          c.oauth_token        = config[:oauth_token]
+          c.oauth_token_secret = config[:oauth_token_secret]
         end
 
-        timer(config[:poll_interval]) do
+        @last_search_id = Hash.new
+        @seen = Hash.new { |hash, key| hash[key] = [] }
+
+        Timer(config[:poll_interval]) do
           poll_timeline
           poll_searches
         end
@@ -40,56 +34,52 @@ module Cinch
 
       # The first time this is run, it just gets the most recent tweet and doesn't output it.
       def poll_timeline
-        rescue_exception do
-          opts = @last_id ? { :since_id => @last_id } : {}
-          tl = @twitter.friends_timeline( opts )
-          if tl.any?
-            if @last_id.nil?
-              @last_id = tl[ 0 ].id.to_i
-            else
-              tl.reverse!
-              tl.reverse_each do |tweet|
-                say_tweet tweet
-              end
+        opts = @last_id ? { :since_id => @last_id } : {}
+        tl = ::Twitter.home_timeline(opts)
+        if tl.any?
+          if @last_id.nil?
+            @last_id = tl[ 0 ].id.to_i
+          else
+            tl.reverse!
+            tl.reverse_each do |tweet|
+              say_tweet tweet
             end
           end
         end
       end
 
       def poll_searches
-        rescue_exception do
-          config[:searches].each do |search_term,channels|
-            search = ::Twitter::Search.new( search_term )
-            last_id = @last_search_id[ search_term ]
-            search.since( last_id )
-            fetched = search.fetch
-            if fetched[ 'max_id' ].to_i > last_id
-              @last_search_id[ search_term ] = fetched[ 'max_id' ].to_i
-              fetched[ 'results' ].each do |tweet|
-                say_search_tweet search_term, tweet, channels
-              end
+        config[:searches].each do |search_term,channels|
+          last_id = @last_search_id[search_term]
+          fetched = ::Twitter.search(search_term, since_id: last_id)
+          max_id = fetched.max_by {|f| f.id}
+          max_id &&= max_id.id
+
+          if max_id.to_i > last_id.to_i
+            @last_search_id[search_term] = max_id
+            next if last_id.nil?
+            fetched.each do |tweet|
+              say_search_tweet search_term, tweet, channels
             end
           end
         end
       end
 
-      def clean_text( text )
-        REXML::Text::unnormalize(
-                                 text.gsub( /&\#\d{3,};/, '?' ).gsub( /\n/, ' ' )
-                                 )
+      def clean_text(text)
+        REXML::Text::unnormalize(text.gsub( /&\#\d{3,};/, '?' ).gsub( /\n/, ' ' ))
       end
 
-      def say_tweet( tweet )
+      def say_tweet(tweet)
         tweet_id = tweet.id.to_i
         return  if tweet_id < @last_id
         @last_id = tweet_id
         src = tweet.user.screen_name
         text = clean_text( tweet.text )
-        alert = "[\00300twitter\003] <#{src}> #{text}"
-        channels = config[:channels][ src ] || [ 'Pistos' ]
+        alert = "[#{Format(:bold, "twitter")}] <#{src}> #{text}"
+        channels = config[:channels][src]
         channels.each do |channel|
           if ! @seen[ channel ].include?( tweet_id )
-            Target(channel).send alert
+            Target(channel).safe_send alert
             @seen[ channel ] << tweet_id
             lang, tr = translate( text )
             if lang && tr
@@ -99,7 +89,7 @@ module Cinch
         end
       end
 
-      def say_search_tweet( search_term, tweet, channels = [ 'Pistos' ] )
+      def say_search_tweet( search_term, tweet, channels )
         tweet_id = tweet[ 'id' ].to_i
         src = tweet[ 'from_user' ]
         text = clean_text( tweet[ 'text' ] )
@@ -121,8 +111,8 @@ module Cinch
         end
       end
 
-      def translate( s )
-        return  if ! defined? Mathetes::Plugins::Translate
+      def translate(s)
+        return if !defined? Mathetes::Plugins::Translate
         lang_source = Translate.detect(s, @bot)
         if lang_source && lang_source != 'en'
           tr = Translate.translate( s, lang_source, @bot )
