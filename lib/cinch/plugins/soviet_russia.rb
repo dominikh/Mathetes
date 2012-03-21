f0c321da6e05d592f98e35ce7f365923b7faa03c
Dominik Honnef <dominikh@fork-bomb.org>
2012-03-21 11:07:14 +0100

    mark SovietRussia plugin as broken

--- lib/cinch/plugins/soviet_russia.rb
+++ lib/cinch/plugins/soviet_russia.rb
@@ -4,6 +4,7 @@
 # By Pistos - irc.freenode.net#mathetes
 
 require 'russian-reversal'
+raise "This plugin is currently broken."
 
 module Cinch
   module Plugins
@@ -26,8 +27,9 @@ module Cinch
         @channels = MuPStore.new( "soviet-russia.pstore" )
       end
 
-      match(/sr ([\S])+(?: (.+))?/)
+      match(/sr (\S+)(?: (.+))?/)
       def execute(m, command, args)
+        debug [command, args].inspect
         return if config[:ignored].include?( m.user.nick )
 
         args    = args.split(" ")
@@ -53,39 +55,35 @@ module Cinch
           end
           m.reply "Soviet Russia interval for #{channel} set to #{ int / 60.0 } minutes."
         when 'test'
-          begin
-            reversal = RussianReversal.reverse( args.join(' ') )
-            if reversal && ! reversal.strip.empty?
-              m.reply "#{m.user.nick}: HA!  In Soviet Russia, #{reversal} YOU!"
-            else
-              m.reply "Stuff like that actually happens in Soviet Russia."
-            end
-          rescue Exception => e
-            m.reply e.message
+          reversal = RussianReversal.reverse( args.join(' ') )
+          if reversal && ! reversal.strip.empty?
+            m.reply "#{m.user.nick}: HA!  In Soviet Russia, #{reversal} YOU!"
+          else
+            m.reply "Stuff like that actually happens in Soviet Russia."
           end
         end
       end
-    end
 
-    listen_to :channel, method: :on_channel
-    def on_channel(m)
-      return if config[:ignored].include?(m.user.nick)
-      channel = m.channel.name.downcase
-      channel_init channel
-      @channels.transaction do
-        delta = Time.now.to_i - @channels[channel][:last]
-        if @channels[channel][:active] && delta > @channels[channel][:interval]
-          begin
-            reversal = RussianReversal.reverse( message.text )
-            if reversal && ! reversal.strip.empty?
-              m.reply "#{m.user.nick}: Ha!  In Soviet Russia, #{reversal} YOU!"
-              @channels[channel][:last] = Time.now.to_i
-            else
-              @bot.loggers.debug "No SR for \"#{message}\""
-            end
-          rescue Exception => e
-            if e.message !~ /sentence has no linkages/
-              @bot.loggers.exception(e)
+      listen_to :channel, method: :on_channel
+      def on_channel(m)
+        return if config[:ignored].include?(m.user.nick)
+        channel = m.channel.name.downcase
+        channel_init channel
+        @channels.transaction do
+          delta = Time.now.to_i - @channels[channel][:last]
+          if @channels[channel][:active] && delta > @channels[channel][:interval]
+            begin
+              reversal = RussianReversal.reverse( message.text )
+              if reversal && ! reversal.strip.empty?
+                m.reply "#{m.user.nick}: Ha!  In Soviet Russia, #{reversal} YOU!"
+                @channels[channel][:last] = Time.now.to_i
+              else
+                @bot.loggers.debug "No SR for \"#{message}\""
+              end
+            rescue Exception => e
+              if e.message !~ /sentence has no linkages/
+                @bot.loggers.exception(e)
+              end
             end
           end
         end
@@ -93,4 +91,3 @@ module Cinch
     end
   end
 end
-
