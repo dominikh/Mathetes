2ef0da4aae6808db510d6f3f230fa04bd56602b4
Dominik Honnef <dominikh@fork-bomb.org>
2012-03-23 15:22:41 +0100

    fix github hook

--- lib/cinch/plugins/github_hook.rb
+++ lib/cinch/plugins/github_hook.rb
@@ -6,108 +6,53 @@
 require 'json'
 require 'open-uri'
 require 'cgi'
-require 'eventmachine'
+require 'sinatra/base'
 
 module Cinch
   module Plugins
-    module GitHubHookServer
-      def self.bot=(bot)
-        @bot = bot
-      end
-
-      def say_rev( rev, message, destination )
-        @seen ||= Hash.new
-        s = @seen[destination] ||= Hash.new
-        if ! s[rev]
-          @bot.channel_list.find_ensured(destination).safe_send(message.tr("\n", " "))
-          s[rev] = true
-        end
-      end
-
-      def zepto_url(url)
-        URI.parse('http://z.pist0s.ca/zep/1?uri=' + CGI.escape(url)).read
-      rescue
-      end
-
-      def receive_data(data)
-        begin
-          hash = JSON.parse(data)
-        rescue JSON::ParserError
-          File.open( Time.now.strftime( "github-bad-data-%Y-%m-%d-%H%M.json" ), 'w' ) { |f| f.puts data }
-          raise
-        end
-
-        repo = hash[ 'repository' ][ 'name' ]
-        repos = PStore.new( 'github-repos.pstore' )
-        repos.transaction { @channels = repos[ repo ] }
-
-        commits = hash[ 'commits' ]
-
-        if commits.size < 7
-          # Announce each individual commit
-          commits.each do |cdata|
-            author = cdata[ 'author' ][ 'name' ]
-            message = cdata[ 'message' ].gsub( /\s+/, ' ' )[ 0..384 ]
-            url = zepto_url( cdata[ 'url' ] )
-            # FIXME escape strings
-            text = "[#{Format(:bold, "github")}] [#{Format(:bold, repo)}] <#{Format(:orange, author)}> #{message} #{url}"
-
-            if @channels.nil? || @channels.empty?
-              @bot.channel_list.find_ensured("#mathetes").send "Unknown repo: '#{repo}'"
-            elsif message !~ /^Merge (?:remote )?branch /
-              @channels.each do |channel|
-                say_rev cdata[ 'id' ], text, channel
-              end
-            end
-          end
-        else
-          # Too many commits; say a summary only
-          authors = commits.map { |c| c[ 'author' ][ 'name' ] }.uniq
-          shas = commits.map { |c| c[ 'id' ] }
-          first_url = zepto_url( commits[ 0 ][ 'url' ] )
-          if @channels
-            @channels.each do |channel|
-              @seen ||= Hash.new
-              s = ( @seen[ channel ] ||= Hash.new )
-              shas.each do |sha|
-                s[ sha ] = true
-              end
-              @bot.channel_list.find_ensured(channel).send "[#{Format(:bold, "github")}] [#{Format(:green, repo)}] #{commits.size} commits by: #{Format(:orange, authors.join( ', ' ))}  #{first_url}"
-            end
-          end
-        end
-
-        close_connection
-      end
-    end
-
     class GitHubHook
+      class Server < Sinatra::Base
+        set :port, 9005
+        set :environment, "production"
+        set :logging, nil
+        def self.bot
+          @bot
+        end
+
+        def self.bot=(bot)
+          @bot = bot
+        end
+
+        post '/' do
+          self.class.bot.loggers.debug "Receiving JSON payload"
+          self.class.bot.handlers.dispatch(:github_hook, nil, params[:payload])
+          ""
+        end
+      end
+
       include Cinch::Plugin
       enable_acl
 
       def initialize(*args)
         super
 
+        @seen = {}
         @repos = PStore.new( "github-repos.pstore" )
-        GitHubHookServer.bot = @bot
-        @em_thread = Thread.new do
-          loop do
-            EventMachine::run do
-              EventMachine::start_server '127.0.0.1', 9005, GitHubHookServer
-            end
-            info "*** EventMachine died; restarting ***"
-          end
+        @sinatra_thread = Thread.new do
+          Server.bot = @bot
+          Server.run!
         end
       end
 
       def unregister(*args)
-        @em_thread.kill
+        @sinatra_thread.kill
         super
       end
 
-      match(/github (?:add|sub|subscribe) (\S+)(?: (\S+)?)$/, method: :subscribe)
+      match(/github (?:add|sub|subscribe) (\S+)(?: (\S+))?$/, method: :subscribe)
       match(/github (?:delete|del|rm|remove|unsub|unsubscribe) (\S+)(?: (\S+))?$/, method: :unsubscribe)
       match(/github list (\S+)$/, method: :list)
+      listen_to :github_hook, method: :github_hook
       def subscribe(m, repository, channel = nil)
         @repos.transaction do
           @repos[repository] ||= []
@@ -152,6 +97,67 @@ module Cinch
               m.reply "No github hook subscriptions found."
             end
           end
+        end
+      end
+
+      def github_hook(m, payload)
+        hash = JSON.parse(payload)
+        repo = hash['repository']['name']
+        repos = PStore.new('github-repos.pstore')
+        channels = nil
+        repos.transaction { channels = repos[repo] }
+        channels.map! {|c| Channel(c)}
+
+
+        commits = hash['commits']
+
+        if commits.size < 7
+          # Announce each individual commit
+          commits.each do |cdata|
+            author  = cdata['author']['name']
+            message = cdata['message'].gsub(/\s+/, ' ')[0..384]
+            url     = cdata['url']
+            # FIXME escape strings
+            text = "[#{Format(:bold, "github")}] [#{Format(:bold, repo)}] <#{Format(:orange, author)}> #{message} #{url}"
+
+            if channels.nil? || channels.empty?
+              Channel("#dominikh").send "Unknown repo: '#{repo}'"
+            elsif message !~ /^Merge (?:remote )?branch /
+              channels.each do |channel|
+                say_rev cdata['id'], text, channel
+              end
+            end
+          end
+        else
+          # Too many commits; say a summary only
+          authors = commits.map { |c| c['author']['name'] }.uniq
+          shas = commits.map { |c| c['id'] }
+          first_url = commits[0]['url']
+          if channels
+            channels.each do |channel|
+              shas.each do |sha|
+                mark_as_seen(sha, channel)
+                @seen[sha] = true
+              end
+              channel.send "[#{Format(:bold, "github")}] [#{Format(:green, repo)}] #{commits.size} commits by: #{Format(:orange, authors.join( ', ' ))}  #{first_url}"
+            end
+          end
+        end
+      end
+
+      private
+      def mark_as_seen(rev, where)
+        (@seen[where] ||= {})[rev] = true
+      end
+
+      def seen?(rev, where)
+        (@seen[where] ||= {})[rev]
+      end
+
+      def say_rev(rev, message, destination)
+        if !seen?(rev, destination)
+          destination.send(message.gsub("\n", ' '))
+          mark_as_seen(rev, destination)
         end
       end
     end
