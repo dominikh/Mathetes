2f96127e26cd3d0509c182da77c89f25b7dbe685
Dominik Honnef <dominikh@fork-bomb.org>
2012-03-25 18:50:05 +0200

    rescue all exceptions in eval and truncate overlong results

--- lib/cinch/plugins/eval.rb
+++ lib/cinch/plugins/eval.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 module Cinch
   module Plugins
     class Eval
@@ -6,8 +7,16 @@ module Cinch
 
       match(/eval (.+)/)
       def execute(m, string)
-        ret = eval(string)
-        m.reply ret.inspect
+        begin
+          ret = eval(string).inspect
+          if ret.size > 400
+            ret = ret[0, 400] + "…"
+          end
+
+          m.reply ret
+        rescue Exception => e
+          m.reply "Exception: #{e.message}"
+        end
       end
     end
   end
